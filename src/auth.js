21fd5e6c442f4f463ad6036ffc1e2fd154e83e55
nellyXinwei <nelsonalbajr@yahoo.com>
2022-09-28 14:14:19 +0800

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,25 +1,25 @@
-import firebase from './firebase';
-import store from './store';
-import db from './db';
-
+import firebase from '@/firebase';
+import store from '@/store';
+import db from '@/db';
+import router from '@/router';
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     if (user.user) {
+      /* eslint-disable */
       user = user.user;
+      /* eslint-enable */
     }
     const setUser = {
       id: user.uid,
       name: user.displayName,
-      email: user.email,
       image: user.photoURL,
-      createdAt: firebase.firestore.FieldValue.serverTimestamp()
-    }
-
+      created_at: firebase.firestore.FieldValue.serverTimestamp(),
+    };
     db.collection('users').doc(setUser.id).set(setUser);
     store.commit('auth/setUser', setUser);
+    router.push('/subreddits');
   } else {
-    // no user is signed in
     store.commit('auth/setUser', null);
   }
-})
+});
